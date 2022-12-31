425a1719b8cb5893a21018514801944de5d1f8b0
Dieal <diealblack2005@gmail.com>
2022-12-31 14:13:31 +0100

    general improvements

--- dwm/install.sh
+++ dwm/install.sh
@@ -2,12 +2,25 @@ mkdir -p programs
 
 read -p 'Do you want to install dwm? (y/n): ' dwm
 if [ $dwm == 'y' ]; then
-    git clone git://git.suckless.org/dwm $HOME/programs/dwm
+
+    # Checks if the dwm folder already exists
+    if [ ! -d "$HOME/programs/dwm" ]; then
+        git clone git://git.suckless.org/dwm $HOME/programs/dwm
+    else
+        cd $HOME/programs/dwm
+        git pull --quiet
+    fi
+
     cp $HOME/.dotfiles/dwm/config.h $HOME/programs/dwm
     cd $HOME/programs/dwm
     make
     sudo make install
 
-    echo -e "\n#DWM Execution\nexec dwm" >> $HOME/.dotfiles/x/.xinitrc
-    echo -e "DWM successfuly installed\n"
+    # Checks that the command above ran successfully
+    if [ $? -eq 0 ]; then
+        echo -e "\n#DWM Execution\nexac dwm" >> $HOME/.dotfiles/x/.xinitrc
+        echo -e "\nDWM successfully installed\n"
+    else
+        echo -e "\nDWM was not installed"
+    fi
 fi
--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 
 ###################### Boilerplate and preparation ######################
 
+# Repository update
+git pull --quiet
+
 # Function that checks if a command exists
 function check_prog() {
     if ! hash "$1" > /dev/null 2>&1; then
@@ -11,21 +14,23 @@ function check_prog() {
     fi
 }
 
-
-mkdir -p "$HOME/.config"
-
-#########################################################################
-
 # Check if the script is running from ~/.dotfiles/
 if [[ ! $(pwd) == $HOME/.dotfiles ]]; then
     echo "Run the script from ~/.dotfiles/"
     exit
 fi
 
+mkdir -p "$HOME/.config"
+
+#########################################################################
+
 # Install needed programs
 # (Edit 'programs.txt' to decide which programs to install)
-sudo pacman -Sy
-sudo pacman -S --needed - < programs.txt
+read -p "Do you want to install the programs specified in programs.txt? (y/n): " programs
+if [ $programs == "y" ]; then
+    sudo pacman -Sy
+    sudo pacman -S --needed - < programs.txt
+fi
 
 check_prog stow
 check_prog curl
@@ -36,13 +41,19 @@ dwm/install.sh
 
 cd $HOME/.dotfiles/
 
-# Install config files with stow
-configs="alacritty scripts fonts xdg bash newsboat x"
-stow --target "$HOME" --no-folding $configs 
+read -p "Do you want to install the config files specified in configs.txt? (y/n): " dotf_answer
+if [ $dotf_answer == "y" ]; then
+    # Install config files with stow
+    stow --target "$HOME" --no-folding $(cat configs.txt)
+
+    if [ $? -eq 0 ]; then
+        echo -e "\n[!] Stow ran into conflicts with some files"
+    fi
+fi
 
 # Install nvim config
 if [ ! -d "$HOME/.config/nvim" ]; then
     git clone https://github.com/Dieal/nvim-config.git $HOME/.config/nvim
 else
-    echo "[!] Neovim configuration already exists"
+    echo -e "\n[!] Neovim configuration already exists"
 fi
